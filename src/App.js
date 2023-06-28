041ae9d063ac43472a46b434d111c170a206dfb1
AsamShivaKumar <asamshivakumar@gmail.com>
2023-06-28 16:46:54 +0530

    Fixed translation issue

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ function App() {
   const [text, setText] = useState("");
   const [translatedText, setTranslatedText] = useState("");
   const [url, setUrl] = useState(null);
+  const [translating, setTranslating] = useState(false);
+
+  // translate("Hello world", "en", "hi").then(res => console.log(res));
 
   function translateText() {
     if(text === "") return;
-    fetch(`https://api.mymemory.translated.net/get?q={text}&langpair=en|hi`)
+    // console.log(`http://localhost:4000/translate?text={text}`);
+    setTranslating(true);
+    fetch(`https://tts-backend.onrender.com/translate?text=${text}`)
     .then(res => res.json())
-    .then(data => setTranslatedText(data.responseData.translatedText))
+    .then(data => setTranslatedText(data.output))
     .catch(err => console.log(err));
-    
+    setTranslating(false);    
   }
 
   function getSpeech(){
     if(translatedText === "") return;
-    fetch(`https://api.voicerss.org/?key=${apiKey}&hl=en-us&src=${translatedText}&r=0&c=mp3&f=44khz_16bit_stereo`)
+    fetch(`https://api.voicerss.org/?key=${apiKey}&hl=hi-in&src=${translatedText}&r=0&c=mp3&f=44khz_16bit_stereo`)
     .then(res => res.blob())
     .then(blob => {
       // play the audio
@@ -38,7 +43,8 @@ function App() {
             <p>Enter your English text here</p>
             <input type="text" placeholder="type here...." value={text} onChange={(evt) => setText(evt.target.value)}/>
             <button onClick={translateText}>Translate to hindi</button>
-            {translatedText !== "" && <p>Translated text: {translatedText}</p>}
+            {translating && <p>Translating ...</p>}
+            {translatedText !== "" && !translating && <p>Translated text: {translatedText}</p>}
             <div>
               <i className="fi fi-ss-play-circle" onClick={getSpeech}></i>
               {url && <a download="audio.mp3" href={url}><i className="fi fi-ss-download"></i></a>}
